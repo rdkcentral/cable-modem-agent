eb0635bc16400c843b134d158dcb098c4ef676c5
Nithishkumar-T <109725053+Nithishkumar-T@users.noreply.github.com>
2025-07-28 15:26:53 +0530

    Update run_ut.sh

--- source/test/run_ut.sh
+++ source/test/run_ut.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 ####################################################################################
-# Copyright 2024 Comcast Cable Communications Management, LLC
+# If not stated otherwise in this file or this component's Licenses.txt file the
+# following copyright and licenses apply:
+#
+# Copyright 2024 RDK Management
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,10 +16,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-# SPDX-License-Identifier: Apache-2.0
 ####################################################################################
-
 # Define the logging function
 log() {
     local level=$1
